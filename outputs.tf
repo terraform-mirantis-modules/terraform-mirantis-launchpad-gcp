0b8a0b5a510aad0c3f9099b437209c1478a285f2
Dimitar Dimitrov <ddimitrov@mirantis.com>
2023-06-26 10:40:22 -0700

    PRODENG-2248 Settings outputs.tf to match the established pattern
    Signed-off-by: Dimitar Dimitrov <ddimitrov@mirantis.com>

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "mke_cluster" {
   value = yamlencode(local.launchpad_tmpl)
 }
+
+output "hosts" {
+  value = concat(local.managers, local.msrs, local.workers, local.windows_workers)
+}
+
+output "cluster_name" {
+  value = local.cluster_name
+}
+
+output "mke_lb" {
+  value = module.managers.lb_public_ip_address
+}
+
+output "msr_lb" {
+  value = module.msrs.lb_public_ip_address
+}
